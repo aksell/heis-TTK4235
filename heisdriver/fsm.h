49803a7191214ad9ff8850f38cbaf5395725fef8
Aksel Lenes <akslenes@gmail.com>
2018-02-16 15:14:40 +0100

    Update fsm.h

--- heisdriver/fsm.h
+++ heisdriver/fsm.h
@@ -1,7 +1,13 @@
+#include "elev.h"
+#include "orderSys.h" 
+#include <stdio.h>
+
 #infdef FSM_H
 #define FSM_H
 
-typedef enum state {
+
+
+typedef enum state_type {
     IDLE = 0;
     MOVING =1;
     STOP = 3;
@@ -9,5 +15,43 @@ typedef enum state {
 
 
 }
+
+
+static state_type current_state;
+static int current_floor;
+static int motor_dir;
+static int previous_floor;
+
+
+
+
+
+int get_current_floor();
+
+int get_motor_dir();
+
+
+
+void set_order_lights();
+
+void set_request_lights();
+
+void set_floor_lights();
+
+
+void open_door();
+
+
+
+//events
+void fsm_init();
+
+void fsm_ev_floor_sensor(int floor);
+
+void fsm_ev_button();
+
+
+
+
 
 #endif
