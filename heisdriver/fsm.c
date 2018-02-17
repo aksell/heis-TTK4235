9d11fd5a1ebabd182d62c618c35429fd33f58c29
Mai Ve Bugge <mv-bugge@hotmail.com>
2018-02-17 15:38:11 +0100

    implemented state machine

--- heisdriver/fsm.c
+++ heisdriver/fsm.c
@@ -1,96 +1,128 @@
 #include "fsm.h"
 
-
-
-int get_current_floor(){
-	return current_floor;
-}
-
-int get_motor_dir(){
-	return motor_dir;
-}
-
-
-
-void set_order_lights(){
-    for(int i = 0; i < 4; i++){
-    	elev_set_button_lamp(BUTTON_COMMAND , i, get_order_list[i]);
-    }
-}
-
-
-void set_request_lights(){
-    for(int i = 0; i < N_BUTTONS; i++){
-    	for(int j = 0; j < 2; j++)
-    		elev_set_button_lamp(i, j, get_request_list(i,j));
-    }
-}
-
-void set_floor_lights(){
-		elev_set_button_lamp(current_floor);
-}
-
-void open_door(){
-
-}
-
-
-
-
-
-void fsm_init(){
+bool fsm_init(){		//Set elev to known current_state//TESTED
+	elev_set_motor_direction(DIRN_UP);
+    while(elev_get_floor_sensor_signal()==-1){};        //aksel vil gi feilmelding etter timer
+    elev_set_motor_direction(DIRN_STOP);
+    current_state = EMERGENCY;
+    return 1;
 	
 }
 
 
 void fsm_ev_floor_sensor(int floor){
 	
-	if(floor != -1 && previous_floor != floor){
-	previous_floor = floor;
-	set_floor_lights(floor);
- 	}
+	if(floor != -1 && previous_floor != floor){		//updates floor
+		previous_floor = floor;
+		elev_set_floor_indicator(previous_floor);
+ 	};
  	current_floor = floor;
 
-	switch (current_state):
+	switch (current_state){
 
 		case IDLE:
-
 			break;
 
 		case MOVING:
-			if(get_should_stop(current_floor, motor_dir)){
+		
+			motor_dir = order_get_dir();
+			elev_set_motor_direction(order_get_dir());
+
+			if(order_should_stop(previous_floor, motor_dir)){
 				current_state = STOP;
 			}
-			
-		}
-		break;
+			break;
 
 		case STOP:
-			open_door();
-			order_executed(current_floor);
+		
+			elev_set_motor_direction(DIRN_STOP);
+			elev_set_door_open_lamp(1);
+			order_executed(previous_floor); 			//Delete order and update orders
+			//wait 3 sec check for emergency
+			for(elev_button_type_t b = BUTTON_CALL_UP; b<=BUTTON_COMMAND; b++){
+				elev_set_button_lamp(b , previous_floor, 0);
+			};
+			elev_set_door_open_lamp(0);
 
-			set_door_lights();
-			close_door();
-			reset_door_lighs();
+			if(no orders){
+				current_state = IDLE;
+			}else{
+				current_state=MOVING;
+			}
+			break;
+			
 
+		case EMERGENCY:
+			elev_set_door_open_lamp(1);
+			while(elev_get_stop_signal()){};
+			elev_set_stop_lamp(0);
+			//timer 3 sec
+			elev_set_door_open_lamp(0);
+			current_state = IDLE;
+			break;
+
+		}
+
+
+}
+
+
+void fsm_ev_emergency(){
+	elev_set_motor_direction(DIRN_STOP);
+	order_clear();
+	elev_set_stop_lamp(1);
+	//clear order + request lights
+
+	switch (current_state){
+
+		case IDLE:
+		case MOVING:
+		case STOP:
+			current_state = EMERGENCY;
 			break;
 
 		case EMERGENCY:
+			//KAN FLYTTES UDER FSM_EV_EMERGENCY
+			while(elev_get_stop_signal()){};
+			elev_set_stop_lamp(0);
+			current_state = IDLE;
+			break;
+	}
+
+};
+
+//because IDLE can either be at floor or no floor
+
+
+void fsm_ev_button(elev_button_type_t button, int floor){
+	update_orders(button, floor);
+	elev_set_button_lamp(button, floor, 1);
+
+	switch (current_state){
+
+		case IDLE:
+			current_state = MOVING;
+
+		case MOVING:
+			motor_dir = order_get_dir();
+			elev_set_motor_direction(order_get_dir());
+			break;
+
+		case STOP:
+			current_state = STOP;
+			break;
+
+		case EMERGENCY:
+			current_state = EMERGENCY;
+			break;
+
+		}
 
 }
 
 
-void fsm_ev_button(){
-
-
-}
-
-	
 
 
 
 
 
-
-
-}
