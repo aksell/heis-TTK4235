76328d3ed27cb95709e4f83f8ca48e8813eb0587
Mai Ve Bugge <mv-bugge@hotmail.com>
2018-03-02 12:48:50 +0100

    Add time_get, timer_set and timer_active, remove old timer_wait

--- heisdriver/timer.c
+++ heisdriver/timer.c
@@ -1,6 +1,24 @@
 #include "timer.h"
 
+void timer_set(){
+	timer = time(0);
+	active = 1;
+};
 
+time_t timer_get(){
+	return (time(0)-timer);
+};
+
+bool timer_active(){
+	return active;
+};
+
+void timer_reset(){
+	active = 0;
+};
+
+
+/*
 void timer_wait(int wait_sec){
 	time_t start_time = time(0);
 
@@ -8,3 +26,4 @@ void timer_wait(int wait_sec){
 	}
 
 };
+*/
--- heisdriver/timer.h
+++ heisdriver/timer.h
@@ -1,9 +1,16 @@
 #include <time.h>
+#include <stdbool.h>
 
 #ifndef TIMER_H
 #define TIMER_H
 
+static time_t timer;
+static bool active;
 
-void timer_wait(int wait_sec);
+
+void timer_set();
+time_t timer_get();
+void timer_reset();
+bool timer_active();
 
 #endif
